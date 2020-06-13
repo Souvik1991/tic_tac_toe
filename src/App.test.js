2f4dcafc2ba5eaa4b358db5c480dde128d131102
Souvik Maity <souvik.maity200@gmail.com>
2020-06-13 19:56:14 +0530

    after adding a app rendering unit test

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,17 @@
 import React from 'react';
 import { render } from '@testing-library/react';
-import App from './App';
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+import { Provider } from 'react-redux';
+
+import Game from "./pages/game";
+import store from "./components/store";
+
+test('tic tac toe', () => {
+	const { getByText } = render(
+		<Provider store={store}>
+			<Game/>
+		</Provider>
+	);
+	const linkElement = getByText(/tic tac toe/i);
+	expect(linkElement).toBeInTheDocument();
 });
